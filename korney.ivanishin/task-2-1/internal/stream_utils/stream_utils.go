9a8443e4ba7de4a661896ab2d0ce2eaea8895438
Korney Ivanishin <korney059@gmail.com>
2025-02-25 15:57:43 +0300

    [TASK-2-1] Fix for loop syntax in stream_utils package

--- korney.ivanishin/task-2-1/internal/stream_utils/stream_utils.go
+++ korney.ivanishin/task-2-1/internal/stream_utils/stream_utils.go
@@ -20,5 +20,5 @@ func ScanInt32() (int32, error) {
 
 func FlushInput() {
         var flushStr string
-        for nFlushed := 1; nFlushed != 0; nFlushed, _ = fmt.Scanln(&flushStr) {}
+        for nFlushed := 1 ; nFlushed != 0 ; nFlushed, _ = fmt.Scanln(&flushStr) {}
 }
