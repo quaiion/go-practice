5d43e801f81a064a5294b43330c6e1dc41209b0d
Korney Ivanishin <korney059@gmail.com>
2025-02-25 20:07:29 +0300

    [TASK-2-2] Add stdin flushing

--- korney.ivanishin/task-2-2/cmd/service/main.go
+++ korney.ivanishin/task-2-2/cmd/service/main.go
@@ -3,29 +3,34 @@ package main
 import (
 	"fmt"
 	"github.com/cafe/internal/process"
+	"github.com/cafe/internal/streamUtils"
 )
 
 func main() {
 	dishHeap, err := process.NewDishHeap()
 	if err != nil {
+		streamUtils.FlushInput()
 		fmt.Println(err)
 		return
 	}
 
 	err = process.ScanDishHeap(dishHeap)
 	if err != nil {
+		streamUtils.FlushInput()
 		fmt.Println(err)
 		return
 	}
 
 	designPos, err := process.ScanDesignPos()
 	if err != nil {
+		streamUtils.FlushInput()
 		fmt.Println(err)
 		return
 	}
 
 	designScore, err := process.GetDesignScore(dishHeap, designPos)
 	if err != nil {
+		streamUtils.FlushInput()
 		fmt.Println(err)
 		return
 	}
